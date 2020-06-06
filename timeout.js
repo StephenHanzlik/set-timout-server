477df43ac4883eba32af928d5af40183e807350c
StephanHanzlik <stephenhanzlik@gmail.com>
2020-06-06 09:33:17 -0600

    adds timeout

--- timeout.js
+++ timeout.js
@@ -5,12 +5,13 @@ var server = http.createServer(function (req, res) {   // 2 - creating server
     //handle incomming requests here..
     if (req.url == '/') { //check the URL of the current request
 
-       // set response header
-       res.writeHead(200, { 'Content-Type': 'text/html' });
-
-       // set response content
-       res.write('<html><body><p>This is home Page.</p></body></html>');
-       res.end();
+      setTimeout(function(){
+        // set response header
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        // set response content
+        res.write('<html><body><p>This is home Page.</p></body></html>');
+        res.end();
+      }, 120000)
 
    }
    else
