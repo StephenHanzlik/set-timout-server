d0a7880d2e7722bbbf3e4bfe3e5f7f67c6803288
StephanHanzlik <stephenhanzlik@gmail.com>
2020-06-06 10:00:32 -0600

    Removed comments

--- timeout.js
+++ timeout.js
@@ -1,18 +1,16 @@
-var http = require('http'); // 1 - Import Node.js core module
+var http = require('http');
 var url = require('url');
 
-var server = http.createServer(function (req, res) {   // 2 - creating server
+var server = http.createServer(function (req, res) {
 
     var timeoutParam = url.parse(req.url, true).query['setTimeout'];
 
       setTimeout(function(){
-        // set response header
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        // set response content
         res.write(`Success with timeout interval ${timeoutParam}`);
         res.end();
       }, timeoutParam)
-
+      
 });
 
 server.listen(process.env.PORT || 5000);
